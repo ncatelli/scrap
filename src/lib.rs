e4b5b66a2fe51fc124bfc6e4f1b4ff9dc7470281
Nate Catelli <ncatelli@packetfire.org>
2020-09-04 20:10:34 -0500

    Minimal App type (#6)
    * Move tests to submod.
    
    * Start stubbing common params
    
    * formatter.
    
    * Remove submodule in test.
    
    * format test.
    
    * Add defaults
    
    * Formatting output.
    
    * Short descriptions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,73 @@
+use std::default;
+use std::fmt;
+
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+mod tests;
+
+/// App functions as the top level wrapper for a command command line tool
+/// storing information about the tool, author, version and a brief description.
+#[derive(Debug, Clone, PartialEq)]
+pub struct App {
+    name: String,
+    author: String,
+    description: String,
+    version: String,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Set the command name.
+    pub fn name(mut self, name: &str) -> App {
+        self.name = name.to_string();
+        self
+    }
+
+    /// Set the author name.
+    pub fn author(mut self, author: &str) -> App {
+        self.author = author.to_string();
+        self
+    }
+
+    /// Set the short description.
+    pub fn description(mut self, desc: &str) -> App {
+        self.description = desc.to_string();
+        self
+    }
+
+    /// Set the version.
+    pub fn version(mut self, vers: &str) -> App {
+        self.version = vers.to_string();
+        self
+    }
+}
+
+impl fmt::Display for App {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let desc = if !self.description.is_empty() {
+            format!("{}\n", &self.description)
+        } else {
+            "".to_string()
+        };
+        let name = if !self.name.is_empty() {
+            format!(" {} ", &self.name)
+        } else {
+            "".to_string()
+        };
+
+        write!(f, "{}Usage:{}[OPTIONS] [SUBCOMMAND]", desc, name)
+    }
+}
+
+impl default::Default for App {
+    fn default() -> Self {
+        App {
+            name: String::new(),
+            author: String::new(),
+            description: String::new(),
+            version: String::new(),
+        }
     }
 }
---
+++ src/tests/mod.rs
@@ -0,0 +1,24 @@
+use crate::App;
+
+#[test]
+fn should_set_app_defaults_on_new() {
+    assert_eq!(
+        App::default(),
+        App::new().name("").description("").author("").version("")
+    );
+}
+
+#[test]
+fn should_match_expected_help_message() {
+    assert_eq!(
+        "this is a test\nUsage: example [OPTIONS] [SUBCOMMAND]",
+        format!(
+            "{}",
+            App::new()
+                .name("example")
+                .description("this is a test")
+                .author("John Doe <jdoe@example.com>")
+                .version("1.2.3")
+        )
+    );
+}
