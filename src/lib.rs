9137c8d657e7de0eb81c943ac344cbd0743f316a
Nate Catelli <ncatelli@packetfire.org>
2020-09-18 02:03:37 +0000

    doc strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+//! # scrap
+//! A terrible command-line parser to satisfy my Not-Invented-Here syndrome for the spasm assembler
+//!
+//! ## Warnings
+//! Please nobody use this. This is entirely an experiment to support insane
+//! restrictions I've imposed on myself for other projects.
+//!
+//! Instead, please use the wonderful [clap.rs](https://github.com/clap-rs/clap)
+//! project that is better supported, better engineered and WAY more mature...
+//! frankly i don't even know why you are even looking at this.
+
 use parcel::{join, one_of, zero_or_more}; // parcel parser combinators
 use parcel::{MatchStatus, ParseResult, Parser};
 use std::collections::HashMap;
--- src/prelude/v1/mod.rs
+++ src/prelude/v1/mod.rs
@@ -1,2 +1,7 @@
+//! This module handles importing all types that should be necessary for any
+//! end user to work with scrap. These work off the assumption that most users
+//! will be interacting via methods defined on the Cmd type or one of it's
+//! derivative types, such as CmdGroup.
+
 pub use crate::flag::{Action, Flag, Value, ValueType};
 pub use crate::Cmd;
