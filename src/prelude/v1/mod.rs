c1f59d8ec952170414c6fd5f13700bde38b4055c
Nate Catelli <ncatelli@packetfire.org>
2021-06-10 20:32:36 +0000

    Add ShortHelpable to prelude

--- src/prelude/v1/mod.rs
+++ src/prelude/v1/mod.rs
@@ -1,4 +1,11 @@
+/// Defines behaviors for traits that can default to a if not specified value.
 pub use crate::Defaultable;
+
+/// Defines behaviors for types that can dispatch an evaluator to a function.
 pub use crate::Dispatchable;
+
+/// Defines behaviors for evaluating an input to a given type.
 pub use crate::Evaluatable;
-pub use crate::Helpable;
+
+/// Defines behaviors for associating help strings with a given type.
+pub use crate::{Helpable, ShortHelpable};
