8e0b15291bf29ea3eb47e912803130a4254bac14
Nate Catelli <ncatelli@packetfire.org>
2021-11-21 18:35:11 +0000

    Examples and docs.

--- examples/basic.rs
+++ examples/basic.rs
@@ -5,51 +5,60 @@ fn main() {
     let raw_args: Vec<String> = env::args().into_iter().collect::<Vec<String>>();
     let args = raw_args.iter().map(|a| a.as_str()).collect::<Vec<&str>>();
 
-    // flag definitions
+    // The `Flag` type defines helpers for generating various common flag
+    // evaluators.
+    // Shown below, the `help` flag represents common boolean flag with default
+    // a default value.
     let help = scrap::Flag::store_true("help", "h", "output help information.")
         .optional()
         .with_default(false);
-    let test = scrap::Flag::store_true("test", "t", "a test flag.")
-        .optional()
-        .with_default(false);
-    let log_level = scrap::Flag::with_choices(
-        "log-level",
-        "l",
-        "logging level.",
-        ["info".to_string(), "warn".to_string(), "error".to_string()],
+    // `direction` provides a flag with a finite set of choices, matching a
+    // string value.
+    let direction = scrap::Flag::with_choices(
+        "direction",
+        "d",
+        "a cardinal direction.",
+        [
+            "north".to_string(),
+            "south".to_string(),
+            "east".to_string(),
+            "west".to_string(),
+        ],
         scrap::StringValue,
     );
 
-    let cmd = scrap::Cmd::new("basic")
-        .description("this is a test")
+    // `Cmd` defines the named command, combining metadata without our above defined command.
+    let cmd = scrap::Cmd::new("minimal")
+        .description("A minimal example cli.")
         .author("John Doe <jdoe@example.com>")
         .version("1.2.3")
         .with_flag(help)
-        .with_flag(test)
-        .with_flag(log_level)
-        .with_handler(|((_, test), _)| {
-            if test {
-                Ok(())
+        .with_flag(direction)
+        // Finally a handler is defined with its signature being a product of
+        //the cli's defined flags.
+        .with_handler(|(_, direction)| println!("You chose {}.", direction));
+
+    // The help method generates a help command based on the output rendered
+    // from all defined flags.
+    let help_string = cmd.help();
+
+    // Evaluate attempts to parse the input, evaluating all commands and flags
+    // into concrete types which can be passed to `dispatch`, the defined
+    // handler.
+    let res = cmd
+        .evaluate(&args[..])
+        .map_err(|e| e.to_string())
+        .and_then(|(help, direction)| {
+            if help {
+                Err("output help".to_string())
             } else {
-                Err("Test is false".to_string())
+                cmd.dispatch((help, direction));
+                Ok(())
             }
         });
 
-    let help_string = cmd.help();
-    let eval_res =
-        cmd.evaluate(&args[..])
-            .map_err(|e| e.to_string())
-            .and_then(|((help, test), log_level)| {
-                if help {
-                    println!("{}", &help_string);
-                    Ok(())
-                } else {
-                    cmd.dispatch(((help, test), log_level))
-                }
-            });
-
-    match eval_res {
-        Ok(_) => println!("Test is true"),
-        Err(e) => println!("error: {}\n\n{}", &e, &help_string),
+    match res {
+        Ok(_) => (),
+        Err(_) => println!("{}", help_string),
     }
 }
